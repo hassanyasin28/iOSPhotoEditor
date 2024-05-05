5aa0c70178ed1af7bcfc7f5871af307983b8e03e
Hassan <hassanyasin28@gmail.com>
2024-05-06 00:03:51 +0500

    fixed black background for png image when it was edited
    Fixed an issue when user tries to edit Png image without background and add test or draws anything, it saved the image with black background everytime

--- Example/Pods/iOSPhotoEditor/Photo Editor/Photo Editor/UIView+Image.swift
+++ Example/Pods/iOSPhotoEditor/Photo Editor/Photo Editor/UIView+Image.swift
@@ -13,7 +13,7 @@ extension UIView {
      Convert UIView to UIImage
      */
     func toImage() -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0.0)
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0.0)
         self.drawHierarchy(in: self.bounds, afterScreenUpdates: false)
         let snapshotImageFromMyView = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
